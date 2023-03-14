7bdffddc2ab0339862f47c4e78f822f75bbcf151
sny430 <shinya430.g@gmail.com>
2023-03-14 12:53:19 +0900

    :sparkles: sort post

--- utils/slice.go
+++ utils/slice.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"hackz-allo/database"
+	"math"
+)
+
 func RemoveFromSlice(strings []string, search string) []string {
 	result := []string{}
 	for _, v := range strings {
@@ -8,4 +13,16 @@ func RemoveFromSlice(strings []string, search string) []string {
 		}
 	}
 	return result
+}
+
+func SortPost(posts []database.Post, limit int) []database.Post {
+	l := len(posts)
+	for i := 0; i < l-1; i++ {
+		for j := i + 1; j < l; j++ {
+			if posts[i].Time > posts[j].Time {
+				posts[i], posts[j] = posts[j], posts[i]
+			}
+		}
+	}
+	return posts[0:int(math.Min(float64(l), float64(limit)))]
 }
