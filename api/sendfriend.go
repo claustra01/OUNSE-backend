f0d41c3e72d6bd2fc1f2884fd9e85ab740461e4d
sny430 <shinya430.g@gmail.com>
2023-03-14 11:42:40 +0900

    :sparkles: send friend api

--- api/sendfriend.go
+++ api/sendfriend.go
@@ -1,11 +1,47 @@
 package api
 
 import (
+	"hackz-allo/database"
+	"hackz-allo/utils"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/exp/slices"
 )
 
 func SendFriend(c echo.Context) error {
+
+	type json struct {
+		User   string `json:"user_id"`
+		Friend string `json:"friend_id"`
+	}
+
+	db := database.Connect()
+
+	// クエリ展開
+	o := new(json)
+	if err := c.Bind(o); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	user := o.User
+	friend := o.Friend
+
+	// レコード取得
+	recu := new(database.Friend)
+	db.Where("user_id = ?", user).First(&recu)
+	recf := new(database.Friend)
+	db.Where("user_id = ?", user).First(&recf)
+
+	// 追加して保存
+	if slices.Contains(recf.RequestUser, user) {
+		recu.FriendUser = append(recu.FriendUser, friend)
+		recf.FriendUser = append(recf.FriendUser, user)
+		recf.RequestUser = utils.RemoveFromSlice(recf.RequestUser, user)
+	} else {
+		recu.RequestUser = append(recu.RequestUser, friend)
+	}
+	db.Save(&recu)
+	db.Save(&recf)
+
 	return c.JSON(http.StatusOK, nil)
 }
