b7d3e44a35d148949c4f5fea2924df20cce1ca94
sny430 <shinya430.g@gmail.com>
2023-03-14 11:20:28 +0900

    :sparkles: create remove from slice function

---
+++ utils/slice.go
@@ -0,0 +1,11 @@
+package utils
+
+func RemoveFromSlice(strings []string, search string) []string {
+	result := []string{}
+	for _, v := range strings {
+		if v != search {
+			result = append(result, v)
+		}
+	}
+	return result
+}
