f16d3a9d4f3b81b81886d2f61cd70a94243b6fba
sny430 <shinya430.g@gmail.com>
2023-03-14 12:53:33 +0900

    :boom: get timeline api

--- api/gettimeline.go
+++ api/gettimeline.go
@@ -1,11 +1,41 @@
 package api
 
 import (
+	"hackz-allo/database"
+	"hackz-allo/utils"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func GetTimeLine(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
+
+	type response struct {
+		posts []database.Post
+	}
+
+	db := database.Connect()
+	user := c.QueryParam("user_id")
+
+	obj := new(response)
+	obj.posts = []database.Post{}
+
+	// フレンド情報取得
+	rec := new(database.Friend)
+	db.Where("user_id = ?", user).First(&rec)
+	friends := rec.FriendUser
+
+	// 投稿取得
+	p := []database.Post{}
+	db.Where("user_id = ?", user).Find(&p)
+	for _, f := range friends {
+		q := []database.Post{}
+		db.Where("user_id = ?", f).Find(&q)
+		p = append(p, q...)
+	}
+
+	// 投稿ソート
+	obj.posts = utils.SortPost(p, 48)
+
+	return c.JSON(http.StatusOK, obj)
 }
